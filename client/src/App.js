f2b50ff69594fd045fc7fe3d7a1fce80e0c075e2
Conor Shirren <cshirren@gmail.com>
2023-03-15 09:33:07 +0000

    feat: add product screen and upgrade to react router v6

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header></Header>
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/login' element={<HomeScreen />} />
+            <Route path='/cart' element={<HomeScreen />} />
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer></Footer>
-    </>
+    </Router>
   );
 };
 
